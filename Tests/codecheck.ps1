a550e5a4433baaa2a3483a37297c9be1ea76c82c
Ben Reader <breader@vigilant.it>
2020-06-14 11:34:26 +1000

    trying to enforce pester

--- Tests/codecheck.ps1
+++ Tests/codecheck.ps1
@@ -2,4 +2,5 @@ $fp = Split-Path $PSScriptRoot -Parent
 if (!(Test-Path $fp\.tests\)) {
     new-item $fp\.tests -ItemType Directory -Force
 }
+Import-Module Pester -RequiredVersion 4.10.1 -Force
 Invoke-Pester -Script "$PSScriptRoot\codecheck.test.ps1" -OutputFile "$fp\.tests\pester.codecheck.test.xml" -OutputFormat 'NUnitXml'
